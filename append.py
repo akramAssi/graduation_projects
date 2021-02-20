47da0390c94974c046df23cc3e91df5ad98e3e9c
NaserShanti <nasershanti654@gmail.com>
2021-02-20 20:33:09 +0200

    Delete append.py

--- append.py
+++
@@ -1,7 +0,0 @@
-    """script to append csv file
-    """
-import pandas as pd
-all_filenames=["Data(R1).csv","Data(R2).csv","Data(R3).csv"]
-combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
-#export to csv
-combined_csv.to_csv( "Data(final).csv", index=False, encoding='utf-8')
